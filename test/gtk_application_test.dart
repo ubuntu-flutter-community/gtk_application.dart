6cd899fc5db9345a3801c6f4efc62ddf6e38ed2d
J-P Nurmi <jp.nurmi@canonical.com>
2022-10-07 23:12:22 +0200

    Test GtkApplication widget

--- test/gtk_application_test.dart
+++ test/gtk_application_test.dart
@@ -1,38 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gtk_application/src/notifier.dart';
+import 'package:gtk_application/src/widget.dart';
 
 import 'test_utils.dart';
 
 void main() {
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  test('command-line', () async {
-    final notifier = GtkApplicationNotifier();
-
+  testWidgets('command-line', (tester) async {
     final receivedArgs = <List<String>>[];
-    notifier.addCommandLineListener(receivedArgs.add);
-
-    await receiveMethodCall('command-line', ['foo', 'bar']);
-    expect(receivedArgs, [
-      ['foo', 'bar']
-    ]);
-
-    await receiveMethodCall('command-line', ['baz qux']);
-    expect(receivedArgs, [
-      ['foo', 'bar'],
-      ['baz qux'],
-    ]);
-
-    receivedArgs.clear();
-    notifier.removeCommandLineListener(receivedArgs.add);
-
-    await receiveMethodCall('command-line', ['none']);
-    expect(receivedArgs, isEmpty);
-  });
-
-  test('open', () async {
-    final notifier = GtkApplicationNotifier();
-
     final receivedFiles = <List<String>>[];
     final receivedHints = <String>[];
 
@@ -41,7 +14,15 @@ void main() {
       receivedHints.add(hint);
     }
 
-    notifier.addOpenListener(receiveOpen);
+    await tester.pumpWidget(GtkApplication(
+      onCommandLine: receivedArgs.add,
+      onOpen: receiveOpen,
+    ));
+
+    await receiveMethodCall('command-line', ['foo', 'bar']);
+    expect(receivedArgs, [
+      ['foo', 'bar']
+    ]);
 
     await receiveMethodCall('open', {
       'files': ['foo', 'bar'],
@@ -52,19 +33,40 @@ void main() {
     ]);
     expect(receivedHints, ['baz']);
 
+    await tester.pumpWidget(const GtkApplication());
+
+    receivedArgs.clear();
+    receivedFiles.clear();
+    receivedHints.clear();
+
+    await receiveMethodCall('command-line', ['none']);
+    expect(receivedArgs, isEmpty);
+  });
+
+  testWidgets('open', (tester) async {
+    final receivedFiles = <List<String>>[];
+    final receivedHints = <String>[];
+
+    void receiveOpen(List<String> files, String hint) {
+      receivedFiles.add(files);
+      receivedHints.add(hint);
+    }
+
+    await tester.pumpWidget(GtkApplication(onOpen: receiveOpen));
+
     await receiveMethodCall('open', {
-      'files': ['baz qux'],
-      'hint': 'quux'
+      'files': ['foo', 'bar'],
+      'hint': 'baz'
     });
     expect(receivedFiles, [
-      ['foo', 'bar'],
-      ['baz qux'],
+      ['foo', 'bar']
     ]);
-    expect(receivedHints, ['baz', 'quux']);
+    expect(receivedHints, ['baz']);
+
+    await tester.pumpWidget(const GtkApplication());
 
     receivedFiles.clear();
     receivedHints.clear();
-    notifier.removeOpenListener(receiveOpen);
 
     await receiveMethodCall('open', {'files': [], 'hint': ''});
     expect(receivedFiles, isEmpty);
